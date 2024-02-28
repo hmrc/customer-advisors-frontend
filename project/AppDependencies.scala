51ec28bc6ecdda5fc09250e14addd2fe12fec5c9
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2024-02-28 10:27:28 +0000

    DC-6070 Tudor crown dependency upgrade (#47)
    * DC-6070 Tudor crown dependency upgrade
    
    * DC-6070 Update plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,5 +1,4 @@
 import play.sbt.PlayImport.ws
-import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
@@ -10,13 +9,11 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "domain-play-30"             % "9.0.0",
     "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "8.3.0",
     "uk.gov.hmrc"            %% "play-partials-play-30"      % "9.1.0",
-    "org.jsoup"              %  "jsoup"                      % "1.17.2",
-    "com.vladsch.flexmark"   %  "flexmark-all"               % "0.35.10" % Test,
-    "org.pegdown"            %  "pegdown"                    % "1.6.0"   % Test,
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"     % "8.4.0"  % Test,
+    "org.jsoup"              % "jsoup"                       % "1.17.2",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30"     % "8.4.0" % Test,
     "org.scalatestplus"      %% "mockito-3-4"                % "3.2.1.0" % Test,
-    "org.scalacheck"         %% "scalacheck"                 % "1.14.0"  % Test,
-    "org.mockito"            %  "mockito-core"               % "4.1.0"   % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"         % "5.1.0"   % Test
+    "org.scalacheck"         %% "scalacheck"                 % "1.14.0" % Test,
+    "org.mockito"            % "mockito-core"                % "4.1.0" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"         % "5.1.0" % Test
   )
 }
