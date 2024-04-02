c7b4316d4c19265fffa4317a837cef747bf345f8
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2024-04-02 12:33:26 +0100

    DC-6189 play-frontend-hmrc version upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"            %% "bootstrap-frontend-play-30" % "8.4.0",
     "uk.gov.hmrc"            %% "domain-play-30"             % "9.0.0",
-    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "8.5.0",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "9.0.0",
     "uk.gov.hmrc"            %% "play-partials-play-30"      % "9.1.0",
     "org.jsoup"              % "jsoup"                       % "1.17.2",
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"     % "8.4.0" % Test,
