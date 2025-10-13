65c9a37cecd5c8b9cdf009c63bfd3b09ab4c07a4
tyagikumarvineet <22053543+tyagikumarvineet@users.noreply.github.com>
2025-10-13 16:13:09 +0100

    [DC-7904] Update hmrc uk.gov.hmrc.domain version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
   val dependencies: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrap_version,
-    "uk.gov.hmrc" %% "domain-play-30"             % "12.1.0",
+    "uk.gov.hmrc" %% "domain-play-30"             % "13.0.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.17.0",
     "org.jsoup"    % "jsoup"                      % "1.17.2",
     compilerPlugin(
