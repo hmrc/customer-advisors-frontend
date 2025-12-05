28e28b58f9ebd204ba2fd75c4a22163c0d41d319
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2025-12-05 15:36:36 +0000

    DC-8176 Migrate customer-advisors-frontend to use service navigation component (#55)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrap_version,
     "uk.gov.hmrc" %% "domain-play-30"             % "13.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.17.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.22.0",
     "org.jsoup"    % "jsoup"                      % "1.17.2",
     "uk.gov.hmrc" %% "bootstrap-test-play-30"     % bootstrap_version % Test
   )
