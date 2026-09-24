f0df9f995807c2a6f072d65d2b66d0171e95d648
Vineet Tyagi <22053543+tyagikumarvineet@users.noreply.github.com>
2026-09-24 11:54:03 +0100

    [DC-9270] - GOV.UK Brand Refresh (#61)
    * [DC-9270] Update license header text and upgdate play frontend version
    
    * [DC-9270] Update license header year to 2026
    
    * [DC-9270] Add default port, same as in service-manager-config
    
    * [DC-9270] Add license header text

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,15 +1,32 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import play.sbt.PlayImport.ws
 import sbt.*
 
 object AppDependencies {
 
   val bootstrap_version = "10.7.0"
+  val playFrontendHmrcVersion = "13.14.0"
 
   val dependencies: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrap_version,
     "uk.gov.hmrc" %% "domain-play-30"             % "13.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "13.13.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % playFrontendHmrcVersion,
     "org.jsoup"    % "jsoup"                      % "1.17.2",
     "uk.gov.hmrc" %% "bootstrap-test-play-30"     % bootstrap_version % Test
   )
